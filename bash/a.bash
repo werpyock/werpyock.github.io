0a18dcdec93ec8cd28067f7b721f677bafd75f0a
Werpyock <ttar2571@gmail.com>
2025-03-04 20:35:23 +0400

    Update a.bash

--- bash/a.bash
+++ bash/a.bash
@@ -4,16 +4,16 @@ loading_animation() {
     local pid=$1
     local delay=0.1
     local spinstr='|/-\'
-    echo -e "\e[?25l"
+    echo -ne "\e[?25l"
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
         local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
+        printf "[%c]" "$spinstr"
         local spinstr=$temp${spinstr%"$temp"}
         sleep $delay
-        printf "\b\b\b\b\b\b"
+        printf "\b\b\b"
     done
-    printf "    \b\b\b\b"
-    echo -e "\e[?25h"
+    printf "   \b\b\b"
+    echo -ne "\e[?25h"
 }
 
 (sleep 10) &
