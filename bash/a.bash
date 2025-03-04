cf6d603f5da7035be04c128b64e13c9f367a6b56
Werpyock <ttar2571@gmail.com>
2025-03-04 20:29:53 +0400

    Update a.bash

--- bash/a.bash
+++ bash/a.bash
@@ -4,7 +4,7 @@ loading_animation() {
     local pid=$1
     local delay=0.5
     local spinstr='|/-\'
-    tput civis
+    echo -e "\e[?25l"
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
         local temp=${spinstr#?}
         printf " [%c]  " "$spinstr"
@@ -13,8 +13,9 @@ loading_animation() {
         printf "\b\b\b\b\b\b"
     done
     printf "    \b\b\b\b"
-    tput cnorm
+    echo -e "\e[?25h"
 }
+
 (sleep 5) &
 task_pid=$!
 
